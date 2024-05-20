9eb5f276e74deee67fe84a628a11ae591d53b74a
aligbakhityar <ahmadbakhtiyar@gmail.com>
2024-05-20 13:47:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const calculateavarage = (arr) => {
+    // const total = arr.reduce((accum, currEle) => accum + currEle, 0)
+    // console.log(total)
+    // return total/ arr.length
+    // const total = arr. sort(function(a, b) {return (a-b)} )
+    // console.log(total)
+    // return total
+    
+
+}
+
+console.log((calculateavarage([5,100,2, 8])))
+
