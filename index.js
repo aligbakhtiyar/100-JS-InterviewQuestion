b32ae8ba4e1a3d89d1db374692a2a289a030838f
aligbakhityar <ahmadbakhtiyar@gmail.com>
2024-05-20 14:24:08 +0530

    12Ques

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 
+//Question:9----------------------------------------
+// const calculateavarage = (arr) => {
+//     const total = arr.reduce((accum, currEle) => accum + currEle, 0)
+//     console.log(total)
+//     return total/ arr.length
+//     const total = arr. sort(function(a, b) {return (a-b)} )
+//     console.log(total)
+//     return total
+// }
+// console.log((calculateavarage([5, 100, 2, 8])))
 
-// Question:9
-const calculateavarage = (arr) => {
-    // const total = arr.reduce((accum, currEle) => accum + currEle, 0)
-    // console.log(total)
-    // return total/ arr.length
-    // const total = arr. sort(function(a, b) {return (a-b)} )
-    // console.log(total)
-    // return total
-    
 
-}
+//Question: 10--------------------------------------
 
-console.log((calculateavarage([5,100,2, 8])))
+// const arrayAreEquals = (arr1, arr2) => {
+//     if (arr1.length != arr2.length)
+//         return false
+//     return arr1.every((curVal, index) => curVal === arr2[index])
+//     }
+// console.log(arrayAreEquals([1, 2, 3], [1, 2, 3]))
+// console.log(arrayAreEquals([1, 2, 3], [1, 2, 4]))
+// console.log(arrayAreEquals([], []))
 
-// Question: 10
+//Question: 11------------- sum of digit
+// const sumofdigits = (num) => {
+//     let arr = Array.from(String(num), Number)
+//     console.log(arr)
+//     let sumofarr = arr.reduce((accum, curEle) => accum + curEle )
+//     return sumofarr
+// }
+// console.log((sumofdigits(1234)))
+// console.log((sumofdigits(12364)))
+// console.log((sumofdigits(12734)))
+
+
+//Question: 12------------- Remove of Duplicate
+const removeDuplicates =(arr) =>{
+    let newArr = [...new Set(arr)]
+    return newAr12r
+12
+
+console.log(remo12veDuplicates([1, 2, 3, 1, 2, 9,3, 4,6 ,5]))
